3b1597be4c198e79fe6b1b35479dd097e7ae7e5a
Sulaimansu <Solvo477@gmail.com>
2026-01-06 01:03:19 +0300

    Define AnimeDatabase with Room database setup

--- app/src/main/java/com/example/offlineanime/data/db/AnimeDatabase.kt
+++ app/src/main/java/com/example/offlineanime/data/db/AnimeDatabase.kt
@@ -1,1 +1,14 @@
+package com.example.offlineanime.data.db
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.offlineanime.data.models.Anime
+import com.example.offlineanime.data.models.UserStatus
+
+@Database(entities = [Anime::class, UserStatus::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AnimeDatabase : RoomDatabase() {
+    abstract fun animeDao(): AnimeDao
+    abstract fun userStatusDao(): UserStatusDao
+}
